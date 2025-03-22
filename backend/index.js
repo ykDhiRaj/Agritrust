8db4651212a9620445bb0813122ea933a8b5e3cc
Dhiraj <feelsnmess@gmail.com>
2025-03-22 14:47:09 +0530

    backend setup

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const cors = require('cors');
+dotenv.config();
+app.use(cors());
+const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
 
-app.listen(3000, () => {});
+mongoose.connect(process.env.MONGODB_URI).
+then(()=>{
+    console.log("Mongo DB connected")
+}).catch((error)=>{
+    console.log(error)
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Successfully connected and app is running at port ${PORT}`)
+})
+
